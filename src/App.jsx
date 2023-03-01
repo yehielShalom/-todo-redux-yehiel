38107004d2c88249d2a9a9ac294f51a721ba833e
Yehiel shalom <y6112000sha@gmail.com>
2023-03-01 11:14:02 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
-import AppRoutes from './routes/appRoutes'
+import React, { useState } from 'react'
 
-const App = () => {
-// Main Compenets 
-  return (
-    <AppRoutes/>
-  )
+export default function App() {
+  let [isCrossed, setIsCrossed] = useState(false);
+  let [winMessage, setWinMessage] = useState();
+
+  let itemArray = new Array(9).fill("empty");
+
+  let reloadGame = () => {
+    setIsCrossed(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+
+  let winSituations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [0, 3, 6],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  
+
+  console.log(itemArray);
+  return <div>App</div>;
 }
-
-export default App
