0f6d3a4ca8288e116f00e027ac0735957ffa23ae
Yehiel shalom <y6112000sha@gmail.com>
2023-03-12 12:42:30 +0200

    f

--- src/components/auth/signUp.jsx
+++ src/components/auth/signUp.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
 
+
+const regexAlphabet = /^[a-zA-Z\s]+$/;
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
 const SignUp = () => {  
     const {getValues,register,handleSubmit,formState: {errors}} = useForm()
     const onSub = (_bodyData) =>{
@@ -25,24 +29,38 @@ console.log(_bodyData)
             <label>First Name:</label>
             <input {...register('firstName',{required:{value:true,message:"first name is required..."}
             ,minLength:{value:2,message:"min 2 char..."},
-            maxLength:{value:20,message:"max 20 chars"}})} type='text' className='form-control' placeholder='Type First Name...' />
+            maxLength:{value:20,message:"max 20 chars"},
+            pattern:{value:regexAlphabet,message:"only alphabet allowed.."}})} type='text' className='form-control' placeholder='Type First Name...' />
         </div>
         <div className='mt-2'>
             <label>Last Name:</label>
-            <input type='text' className='form-control' placeholder='Type Last Name...' />
+            <input {...register('lastName',{required:{value:true,message:"last name is required..."},
+            minLength:{value:2,message:"min 2 char..."},
+            maxLength:{value:20,message:"max 20 chars"},
+            pattern:{value:regexAlphabet,message:"only alphabet allowed.."}})}
+            type='text' className='form-control' placeholder='Type Last Name...' />
         </div>
 
         <div className='mt-2'>
             <label>Email:</label>
-            <input type='email' className='form-control' placeholder='Type Email...' />
+            <input {...register('Email',{required:{value:true, message:"Email its empty"},
+            pattern:{value:regexEmail,message:"u have @"}})}
+             type='email' className='form-control' placeholder='Type Email...' />
         </div>
         <div className='mt-2'>
             <label>Password:</label>
-            <input type='password' className='form-control' placeholder='Type Password...' />
+            <input {...register('Password',{required:{value:true , message:"Password is empty!"},
+            minLength:{value:5,message:"min 5 char..."},
+            maxLength:{value:20,message:"max 20 chars"},
+            })}
+            type='password' className='form-control' placeholder='Type Password...' />
         </div>
         <div className='mt-2'>
             <label>Confirm Password:</label>
-            <input type='password' className='form-control' placeholder='Confirm Password...' />
+            <input {...register('Confrim Password',{required:{value:true , message:"Confirm Password is empty!"},
+            validate:{value:(val)=> getValues('password') == val || "password do not match"}
+            })}
+             type='password' className='form-control' placeholder='Confirm Password...' />
         </div>
         <div className='mt-2'>
         <button className='btn-primary btn me-2'>Sign Up</button>
