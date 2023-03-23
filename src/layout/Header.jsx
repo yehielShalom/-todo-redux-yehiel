61fa38e04aba0de4540a372e37c9b97dd943cc76
Yehiel shalom <y6112000sha@gmail.com>
2023-03-23 14:51:00 +0200

    x

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-    <div className="container">
-    <Link className="navbar-brand" to={"#"}>Navbar</Link>
-    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="collapsibleNavId">
-      <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" to={"/"} aria-current="page">Home <span className="visually-hidden">(current)</span></Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to={"/page1"}>Page1</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to={"/page2"}>Page2</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div className="container">
+        <Link className="navbar-brand" to={"#"}>
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" to={"/"} aria-current="page">
+                Home <span className="visually-hidden">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/page1"}>
+                Page1
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/page2"}>
+                x
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
