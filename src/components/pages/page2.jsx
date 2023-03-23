ed0e95f55bbdbd84ec5e3957845f3d6f4eeadbcd
Yehiel shalom <y6112000sha@gmail.com>
2023-03-23 15:25:28 +0200

    j

--- src/components/pages/page2.jsx
+++ src/components/pages/page2.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../../context/context";
 
 const Page2 = () => {
-  const { count } = useContext(AppContext);
+  const { count} = useContext(AppContext);
   return <div>
     <h1>count:{count}</h1>
     </div>
