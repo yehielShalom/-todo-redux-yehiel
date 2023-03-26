d0328617fc5ca5e4ab3bfb37c2c7811c175b121a
Yehiel shalom <y6112000sha@gmail.com>
2023-03-26 09:43:51 +0300

    s

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,8 +5,6 @@ const Header = () => {
   return (
 <nav className='flex justify-between px-[30px] py-[30px] bg-red-600 text-white'>
 <Link to={'/'}>Home</Link>
-<Link to={'/page1'}>page1</Link>
-<Link to={'/page2'}>page2</Link>
 </nav>
   )
 }
